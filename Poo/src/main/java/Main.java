01fecc3815d4a7946b7243203c0ea553858100b2
AlvaroCareli <122116260+alvarosamp@users.noreply.github.com>
2024-04-26 19:45:21 -0300

    Update Main.java

--- Poo/src/main/java/Main.java
+++ Poo/src/main/java/Main.java
@@ -1,2 +1,25 @@
+import DAO.Connection.DAOConsultas_Medicas;
+import Model.Consultas_medicas;
+
 public class Main {
-}
+
+
+        public static void main(String[] args) {
+
+            DAOConsultas_Medicas consultaDAO = new DAOConsultas_Medicas();
+
+            Consultas_medicas c1 = new Consultas_medicas(2178,3212,"27/09/2003","Dengue","Negativo");
+
+            consultaDAO.insertAluno(c1);
+
+            consultaDAO.selectAluno();
+
+            consultaDAO.updateMotivo(2178, "Dor de cabeça");
+
+            consultaDAO.selectAluno();
+
+            consultaDAO.deleteConsulta(2178);
+
+        }
+    }
+
